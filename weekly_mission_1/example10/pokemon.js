1b5a977caa4244131b0403e84f4c083693914fa4
jricardocam <jricardocam@gmail.com>
2022-04-09 14:55:25 -0500

    Agregar

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,13 @@
+export default class Pokemon {
+    constructor(name) {
+      // this es una variable para referenciar el valor del contexto local de esta clase
+      this.name = name // Estás variables se le conocen como atributos
+    }
+    sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+    sayMessage(message){
+        console.log(`Mi pokemon ${this.name} te dice ${message}`)
+    }
+}
+
