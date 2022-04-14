c542a833eef552a625e494b0c40d9814faa2d06c
jricardocam <jricardocam@gmail.com>
2022-04-13 20:43:49 -0500

    Operadores
    ejemplos con operadores

---
+++ weekly_mission_2/operadores/example_4.js
@@ -0,0 +1,6 @@
+const numbers4 = [1, 2, 3, 4, 5]
+const numbersSquare = numbers4.map(function(num){ return num * num})
+// También puedes escribir la función como fat arrow
+//const numbersSquare = numbers4.map((num) => return num * num)
+console.log("Ejemplo 4: Imprimiendo la lista de elementos al cuadrado")
+console.log(numbersSquare)
---
+++ weekly_mission_2/operadores/example_5.js
@@ -0,0 +1,5 @@
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+
+console.log("Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a minúsculas")
+console.log(namesToUpperCase)
