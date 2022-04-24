bddf8169bde678e5235ef55e67f6c8ef45e4f659
jricardocam <jricardocam@gmail.com>
2022-04-24 12:59:00 -0500

    Agregado ruta para regresar objeto

---
+++ weekly_mission_3/express_basic/app.js
@@ -0,0 +1,26 @@
+//Usando Objeto express
+const express=require("express")
+//App de express
+const app =express()
+//Puerto en que veremos app :localhost:3000
+const port =3000
+
+//path inicial, responderá a la url localhost:3000
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+app.get('/launchx',(req,res)=>{
+    res.send("Bienvenidos a launchx")
+})
+//Regresando un objeto
+app.get('/explorersInNode',(req,res)=>{
+    const explorer={
+        name:"José Ricardo",
+        msg:"Hello"
+    }
+    res.send(explorer)
+})
+//Con esto inicializamos esta app
+app.listen(port,()=>{
+    console.log(`example app listening on port ${port}`)
+})
